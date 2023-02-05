d2acb10affc16131c8907fdefb5448393186f208
Ryebread5555 <Rypetty55@gmail.com>
2023-02-05 17:37:56 -0600

    added html page

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -80,7 +80,7 @@ generateHTML = (data) => {
             const internCard = generateIntern(employee);
 
             pageArray.push(internCard);
-        }
+        };
     }
 
     const employeeCards = pageArray.joint('')
@@ -89,4 +89,39 @@ generateHTML = (data) => {
     return generateTeam;
 };
 
+// function for HTML page
+const generateTeamPage = employeeCards => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
 
+    <head>
+        <meta charset="UTF-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Team Profile</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        <link rel="stylesheet" href="style.css />
+    </head>
+    
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-heading bg-info">
+                <h1 class="text-center text-white">Project Team</h1>
+                </div>
+            </div>
+        </div>
+    
+    <div class="container">
+        <div class="row">
+            <div class="row team-area col-12 d-flex justify-conter-center">
+                ${employeeCards(team)}
+            </div>
+        </div>
+    </div>
+    
+    </body>
+</html>
+`;
+};
