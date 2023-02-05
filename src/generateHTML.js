ffc0fcb20a61c275791fd64ca5fd92968fb23a3f
Ryebread5555 <Rypetty55@gmail.com>
2023-02-05 17:06:21 -0600

    added intern card and added function for HTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -35,3 +35,58 @@ const generateEngineer = engineer => {
 </div>
 `;
 };
+
+// intern card
+const generateIntern = intern => {
+    return `
+    <div class="card employee-card">
+    <div class="card-header bg-success text-white">
+        <h2 class="card-title">${intern.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${intern.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+            <li class="list-group-item">School: ${intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
+`;
+};
+
+generateHTML = (data) => {
+
+    // creates array for cards
+    pageArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole();
+
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+
+            pageArray.push(managerCard);
+        };
+
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+
+            pageArray.push(engineerCard);
+        };
+
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+
+            pageArray.push(internCard);
+        }
+    }
+
+    const employeeCards = pageArray.joint('')
+
+    const generateTeam = generateTeamPage(employeeCards);
+    return generateTeam;
+};
+
+
