9f67452b76ac36ba6c62612011c05d4be7c44db7
Ryebread5555 <Rypetty55@gmail.com>
2023-02-05 15:16:38 -0600

    added engineer card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,7 +2,7 @@
 const generateManager = manager => {
     return `
     <div class="card employee-card">
-    <div class="card-header bg-primary text-white">
+    <div class="card-header bg-success text-white">
         <h2 class="card-title">${manager.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -11,6 +11,25 @@ const generateManager = manager => {
             <li class="list-group-item">ID: ${manager.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
             <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+`;
+};
+
+// engineers card
+const generateEngineer = engineer => {
+    return `
+    <div class="card employee-card">
+    <div class="card-header bg-success text-white">
+        <h2 class="card-title">${engineer.getName()}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
         </ul>
     </div>
 </div>
