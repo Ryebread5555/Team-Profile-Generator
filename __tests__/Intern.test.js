25c20e23abe043573095fc8a20ab93522e10f1ef
Ryebread5555 <Rypetty55@gmail.com>
2023-02-06 16:12:50 -0600

    testSchool wasn't being passed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,18 +2,18 @@ const Intern = require("../lib/Intern");
 
 test("School created", () => {
     const testSchool = "School Name";
-    const employeeHere = new Intern("Ryan", 5, "Northwestern");
+    const employeeHere = new Intern("Ryan", 5, "ryan@gmail.com", testSchool);
     expect(employeeHere.school).toBe(testSchool);
 });
 
 test("officeNumber is printed", () => {
     const testSchool = "School Name";
-    const employeeHere = new Intern("Ryan", 5, "Northwestern");
+    const employeeHere = new Intern("Ryan", 5, "ryan@gmail.com", testSchool);
     expect(employeeHere.getSchool()).toBe(testSchool);
 });
 
 test("Role selected", () => {
     const returnValue = "Intern";
-    const employeeHere = new Intern("Ryan", 5, "Northwestern");
+    const employeeHere = new Intern("Ryan", 5, "ryan@gmail.com", "Northwestern");
     expect(employeeHere.getRole()).toBe(returnValue);
 });
